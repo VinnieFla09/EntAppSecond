22c57acd0483eb0e3acda2a2b49dae5dc9b168bf
Vinny Flaherty <v.flaherty1@gmail.com>
2019-02-19 08:16:43 +0000

    Task 1/2/3 of Validation WS VF 19.02.2019

---
+++ EntAppSecond/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntAppSecond.Models
+{
+    public class Student
+    {
+        [Required]
+        public string StudentID { get; set; } = "";
+
+        [Required]
+        public string FirstName { get; set; } = "";
+
+        [Required]
+        public string LastName { get; set; } = "";
+    }
+}
