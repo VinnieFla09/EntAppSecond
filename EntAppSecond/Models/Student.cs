81dd2ad4ede56b358395c50b7e89c63e66e79aaf
Vinny Flaherty <v.flaherty1@gmail.com>
2019-02-19 09:12:59 +0000

    VF 19.02.2019: WS Validation completed

--- EntAppSecond/Models/Student.cs
+++ EntAppSecond/Models/Student.cs
@@ -8,13 +8,16 @@ namespace EntAppSecond.Models
 {
     public class Student
     {
-        [Required]
+        [Required (ErrorMessage = "Please enter a student ID!")]
         public string StudentID { get; set; } = "";
 
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; } = "";
+       
 
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; } = "";
     }
 }
